40d0fc11cb792abe465436d2e3e2ecdde23fca3c
mosmosyrc <prfm_love2.mosmos@icloud.com>
2018-10-12 12:46:33 +0900

    メニューバーとトップページに載せるタイトルロゴの微調整

--- js/page.js
+++ js/page.js
@@ -16,15 +16,9 @@ function hamburger() {
 	}
 }
 
-$(function clickSpMenu() {
-	$('.nav-content-link > sp-img').on('click', function() {
-		$('#nav-content').
-	})
-})
-
 $(document).ready(function() {
 	hamburger();
-	$(this).on("click", ".top-menu-button, .header-img, .nav-content-link, .nav-content-title-logo",  function() {
+	$(this).on("click", ".top-menu-button, .header-img > img, .nav-content-link, .nav-content-title-logo",  function() {
 		$("article#main_content").empty().html("<p id='loading'>Loading...</p>");
 		$("html, body").scrollTop(0);
 		//menu style change
