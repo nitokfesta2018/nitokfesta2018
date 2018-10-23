f40b4138c30523e13c10553bbaa793a0951793a9
Gatyamin <heyapple7orange7@gmail.com>
2018-10-23 13:19:59 +0900

    タイムテーブル調節完了

--- js/switch.js
+++ js/switch.js
@@ -1,9 +1,9 @@
 /*arinaボタン押したとき*/
 $(document).on("click", "#arina_button",  function() {
 	/*表示・非表示の切り替え*/
-	$('.arina_timetable').css("display", "block");
-	$('.stilt_timetable').css("display", "none");
-	$('.others_timetable').css("display", "none");
+	$('#arina_timetable').css("display", "block");
+	$('#stilt_timetable').css("display", "none");
+	$('#others_timetable').css("display", "none");
 	/*ボタンの色変更*/
 	$(this).css("background-color", "#577342");
 	$("#stilt_button").css("background-color", "#ECE3D4");
@@ -12,9 +12,9 @@ $(document).on("click", "#arina_button",  function() {
 
 /*stiltボタン押したとき*/
 $(document).on("click", "#stilt_button",  function() {
-	$('.stilt_timetable').css("display", "block");
-	$('.arina_timetable').css("display", "none");
-	$('.others_timetable').css("display", "none");
+	$('#stilt_timetable').css("display", "block");
+	$('#arina_timetable').css("display", "none");
+	$('#others_timetable').css("display", "none");
 	$(this).css("background-color", "#577342");
 	$("#arina_button").css("background-color", "#ECE3D4");
 	$("#others_button").css("background-color", "#ECE3D4");	
@@ -22,9 +22,9 @@ $(document).on("click", "#stilt_button",  function() {
 
 /*othersボタン押したとき*/
 $(document).on("click", "#others_button",  function() {
-	$('.others_timetable').css("display", "block");
-	$('.arina_timetable').css("display", "none");
-	$('.stilt_timetable').css("display", "none");
+	$('#others_timetable').css("display", "block");
+	$('#arina_timetable').css("display", "none");
+	$('#stilt_timetable').css("display", "none");
 	$(this).css("background-color", "#577342");
 	$("#arina_button").css("background-color", "#ECE3D4");
 	$("#stilt_button").css("background-color", "#ECE3D4");	
